d46a5043971afe963ca5bc479409278335ca0de7
Sam Blackman <sblackman@gmail.com>
2018-05-22 15:01:00 -0400

    Reorder board and cell functons

--- Game.hs
+++ Game.hs
@@ -11,6 +11,17 @@ evolve board = listArray (bounds board) boardValues
     cellsAndNeighbors  = zip (elems board) boardWithNeighbors
     boardValues        = map evolveCell cellsAndNeighbors
 
+neighbors :: Coordinate -> [Coordinate]
+neighbors (x, y) = [
+
+  (x-1,y-1), (x,y-1), (x+1,y-1),
+  (x-1,y  ),          (x+1,y),
+  (x-1,y+1), (x,y+1), (x+1,y+1)]
+
+isAlive :: Cell -> Bool
+isAlive Alive = True
+isAlive Dead = False
+
 evolveCell :: (Cell, [Cell]) -> Cell
 evolveCell (Alive, neighbors)
   | (countLiving neighbors) == 2 = Alive
@@ -22,18 +33,3 @@ evolveCell (Dead, neighbors)
   | otherwise                    = Dead
 
 countLiving = length . (filter isAlive)
-
-isAlive :: Cell -> Bool
-isAlive Alive = True
-isAlive Dead = False
-
-neighbors :: Coordinate -> [Coordinate]
-neighbors (x, y) = [
-
-  (x-1,y-1), (x,y-1), (x+1,y-1),
-  (x-1,y  ),          (x+1,y),
-  (x-1,y+1), (x,y+1), (x+1,y+1)]
-  
-
-count :: Eq a => a -> [a] -> Int
-count x = length . filter (==x)
